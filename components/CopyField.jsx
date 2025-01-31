11bfd8d93efa79bec6efd1a9340d754647c9bb9a
saboiii <saba.x.azad@gmail.com>
2025-01-31 23:45:20 +0800

    truncate

--- components/CopyField.jsx
+++ components/CopyField.jsx
@@ -12,7 +12,7 @@ function CopyField({content, title}) {
         <div className='flex flex-col gap-2 w-full'>
             <div className='flex ml-1 uppercase font-medium text-gray-600'>{title}</div>
             <div className='flex w-full md:w-2/3 containerFormat text-slate-200  transition duration-200 ease-in-out justify-between'>
-                <div className='flex px-4 py-2'>{content}</div>
+                <div className=' px-4 py-2 w-[80%] block truncate'>{content}</div>
                 <button onClick={() => handleCopy(content)} className='flex items-center justify-center border-l text-[#2b2b2d] hover:text-slate-200 transition duration-500 ease-in-out border-[#1f1f21] cursor-pointer w-10'>
                     <IoMdCopy size={16} className='flex' />
                 </button>
