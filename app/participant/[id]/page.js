013dec4c1487dca7cbd30e1b03aac7563371ca64
saboiii <saba.x.azad@gmail.com>
2025-01-31 23:43:42 +0800

    ui for solo

--- app/participant/[id]/page.js
+++ app/participant/[id]/page.js
@@ -114,7 +114,7 @@ function ParticipantPage() {
             <div className='flex flex-col items-center justify-center w-screen bg-black text-[#eeeeee]'>
 
                 {participant.solo ? (
-                    <div className='flex flex-col h-screen w-screen items-center md:items-start justify-center'>
+                    <div className='flex flex-col h-screen w-screen items-center md:items-start justify-center mt-12'>
                         <div className='flex w-full items-end justify-between md:justify-start gap-4 px-12 md:px-24 mb-8'>
                             <button onClick={handleGoBack} className='rounded-md flex items-center bg-[#151518] border border-[#1f1f21] font-medium uppercase text-[#535357] h-8 pl-2 pr-4 py-2'>
                                 <IoChevronBack className='inline mr-2' />
