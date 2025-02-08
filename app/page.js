b1ffb9d4821ce2bcde7d68ca08485af6e7016f1e
saboiii <saba.x.azad@gmail.com>
2025-02-08 23:59:53 +0800

    changes

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className='flex flex-col w-full h-screen justify-center items-center'>
       <div className="mb-4 title">DLW ADMIN SITE</div>
-      <p className="mb-6">&#40;still in progress, downloads to be added soon :p&#41;</p>
+      <p className="mb-6">&#40;dm me if you want additional features :p&#41;</p>
       <Link href='/dashboard' className="buttonDesign">Dashboard</Link>
     </div>
   );
